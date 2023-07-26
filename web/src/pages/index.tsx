5e6bb780bff0816ad9a1137253db46e33b23b5aa
zhikangfan <zhikangfan@outlook.com>
2023-07-26 09:29:51 +0800

    feat: 添加搜索用户id功能

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -2,8 +2,9 @@ import React, {useEffect, useState} from 'react';
 import {InboxOutlined, UploadOutlined} from "@ant-design/icons";
 import type { UploadProps } from 'antd';
 
-import {message, Table, Upload, Image, Form, Select, Button} from 'antd';
+import {message, Table, Upload, Image, Form, Select, Button, Input} from 'antd';
 const { Dragger } = Upload;
+const { Search } = Input;
 import axios from 'axios'
 
 export interface TableData {
@@ -25,7 +26,7 @@ export default function HomePage() {
 
   const key = 'loadingKey'
   const isLoadZipSwitch = true; // 是否开启懒加载zip，若服务端开启则开启，关闭则关闭
-  let defaultPageSize = 1;
+  let defaultPageSize = 100;
 
   const props: UploadProps = {
     name: 'xlsx',
@@ -66,71 +67,91 @@ export default function HomePage() {
   const [currentPage, setCurrentPage] = useState<number>(1)
   const [total, setTotal] = useState<number>(0)
   const [tableLoading, setTableLoading] = useState<boolean>(false)
+  const [searchFilename, setSearchFilename] = useState<string>('')
 
   const columns = [
     {
       title: "神策ID",
       key: "distinct_id",
+      width: 100,
       dataIndex: "distinct_id"
     },
     {
       title: "用户ID",
       key: "uid",
+      width: 100,
       dataIndex: "uid"
     },
     {
       title: "帧率",
       key: "frame_rate",
+      width: 50,
+      ellipsis: true,
+      textWrap: 'word-break',
       dataIndex: "frame_rate"
     },
     {
       title: "用户状态",
       key: "user_status",
+      width: 80,
+      ellipsis: true,
+      textWrap: 'word-break',
       dataIndex: "user_status"
     },{
       title: "录制进程",
       key: "record_process",
+      width: 100,
+      ellipsis: true,
+      textWrap: 'word-break',
       dataIndex: "record_process"
     },
     {
       title: "录制模式",
       key: "record_mode",
+      width: 100,
+      ellipsis: true,
+      textWrap: 'word-break',
       dataIndex: "record_mode"
     },
     {
-      title: "录制时长（秒）",
+      title: "时长",
       key: "record_duration",
+      width: 80,
       dataIndex: "record_duration"
     },
     {
-      title: "文件大小（Mb）",
+      title: "文件大小",
       key: "file_size",
       dataIndex: "file_size",
-      render(text: number, record: TableData, index: number) {
-        return (text / 1024 / 1024)
-      }
+      width: 100
+      // render(text: number, record: TableData, index: number) {
+      //   return (text / 1024 / 1024)
+      // }
     },
     {
       title: "分辨率",
       key: "resolution",
+      width: 100,
+      ellipsis: true,
+      textWrap: 'word-break',
       dataIndex: "resolution"
     },
 
-    {
-      title: "视频采样帧",
-      key: "files",
-      dataIndex: "files",
-      render(text: Array<string>, record: TableData, index: number) {
-        return <>
-          <Image.PreviewGroup>
-            {
-              record?.files?.map((item,idx) => <Image width={150} key={item+"_"+idx} src={"http://10.10.40.24:3001" + item} />)
-            }
-
-          </Image.PreviewGroup>
-        </>
-      }
-    }
+    // {
+    //   title: "视频采样帧",
+    //   key: "files",
+    //   dataIndex: "files",
+    //   render(text: Array<string>, record: TableData, index: number) {
+    //     return <>
+    //       <Image.PreviewGroup>
+    //         {
+    //           record?.files?.map((item,idx) => <Image width={300} key={item+"_"+idx} src={"http://10.10.40.24:3001" + item} />)
+    //         }
+    //
+    //       </Image.PreviewGroup>
+    //     </>
+    //   }
+    // }
   ]
   const [data, setData] = useState<Array<TableData>>([])
   const [files, setFiles] = useState([])
@@ -155,12 +176,31 @@ export default function HomePage() {
     setTableLoading(true)
     try {
       let res = await axios.get('/data?name=' + filename)
+
       let d = await addPictures(1, defaultPageSize, res.data.data)
       setData(d)
       setTotal(d.length)
       setTableLoading(false)
     } catch (e) {
       message.error('加载数据错误!')
+      setTableLoading(false)
+    }
+  }
+
+  async function onSearch(value: string) {
+    if (value?.trim().length === 0) {
+      await handleSearch(searchFilename.split('.xlsx')[0])
+      return
+    }
+    try {
+      setTableLoading(true)
+      let filterData = data.filter(item => item.uid === value);
+      let d = await addPictures(1, defaultPageSize, filterData)
+      setData(d)
+      setTotal(d.length)
+      setTableLoading(false)
+    } catch (e) {
+      message.error('查找失败!')
       setTableLoading(false)
     }
   }
@@ -200,10 +240,15 @@ export default function HomePage() {
    * @param {number} pageSize
    */
   async function onPageChange(page: number, pageSize: number) {
-    await addPictures(page, pageSize, data)
-    await setCurrentPage(page)
+    setTableLoading(true)
+    try {
+      await addPictures(page, pageSize, data)
+      await setCurrentPage(page)
+      setTableLoading(false)
+    } catch (e) {
+      setTableLoading(false)
+    }
   }
-
   return (
     <div style={{padding: '20px'}}>
 
@@ -216,6 +261,7 @@ export default function HomePage() {
             <Select
               style={{ width: 180 }}
               onChange={ async (filename) => {
+                setSearchFilename(filename)
                 await handleSearch(filename.split('.xlsx')[0])
                 await setCurrentPage(1)
               }}
@@ -224,6 +270,9 @@ export default function HomePage() {
 
           </Form.Item>
         </Form>
+        <div style={{width: 350, marginLeft: 10}}>
+          <Search placeholder="用户ID" onSearch={onSearch} enterButton />
+        </div>
         {/*<div style={{marginLeft: '30px'}}>*/}
         {/*  <Upload {...props}>*/}
         {/*    <Button icon={<UploadOutlined />}>选择文件（.xlsx）</Button>*/}
@@ -240,6 +289,31 @@ export default function HomePage() {
           }
         }
         bordered
+        expandable={{
+          defaultExpandAllRows: true,
+          columnTitle: '视频采样帧',
+          columnWidth: 50,
+          expandedRowRender: (record) => {
+            return <>
+              {/*<div style={{*/}
+              {/*  color: 'rgba(0,0,0,.88)',*/}
+              {/*  fontWeight: 600,*/}
+              {/*  paddingBottom: 15,*/}
+              {/*  marginBottom: 15,*/}
+              {/*  background: '#fafafa',*/}
+              {/*  borderBottom: '1px solid #f0f0f0'*/}
+              {/*}}>*/}
+              {/*  视频采样帧:*/}
+              {/*</div>*/}
+              <Image.PreviewGroup>
+                {
+                  record?.files?.map((item,idx) => <Image width={300} key={item+"_"+idx} src={"http://10.10.40.24:3001" + item}/>)
+                }
+
+              </Image.PreviewGroup>
+            </>
+          },
+        }}
         pagination={{
           total: total,
           defaultPageSize: defaultPageSize,
